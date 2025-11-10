56bf8fc5f9d51774fb5491abfd571a2d42772cfe
vatsalkeshav <=>
2025-11-10 15:00:16 +0530

    done : audio and startup, frames init updation and rendering

---
+++ src/main.rs
@@ -0,0 +1,99 @@
+use std::time::Duration;
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{ExecutableCommand, event, terminal};
+use rusty_audio::Audio;
+use std::error::Error;
+use std::{io, thread};
+use std::sync::mpsc;
+use invaders::{frame, render};
+use invaders::frame::new_frame;
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    // audio
+    let mut audio = Audio::new();
+    audio.add("explode", "original/explode.wav");
+    audio.add("lose", "original/lose.wav");
+    audio.add("move", "original/move.wav");
+    audio.add("pew", "original/pew.wav");
+    audio.add("startup", "original/startup.wav");
+    audio.add("win", "original/win.wav");
+
+    // startup audio
+    audio.play("startup");
+
+    //terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // a loop meant for screen rendering - to be run in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        'rendering: loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break 'rendering,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // 'gameloop: loop {
+    //     // input
+    //     while event::poll(Duration::default())? {
+    //         match event::read()? {
+    //             Event::Key(key_event_mf) => {
+    //                 match key_event_mf.code {
+    //                     KeyCode::Esc | KeyCode::Char('q') => {
+    //                         audio.play("lose");
+    //                         break 'gameloop;
+    //                     }
+    //                     _ => {}
+    //                 }
+    //             }
+    //             _ => {}
+    //         }
+    //     }
+    // }
+    'gameloop: loop {
+        // per-frame init
+        let curr_frame = new_frame();
+
+        //input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event_mf) = event::read()? {
+                match key_event_mf.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // draw and render
+        let _ = render_tx.send(curr_frame); // after startup, for some time, there'd be no receiver available. This `let` is to ignore that thing to avoid a crash
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Cleanup
+    // join the threads
+    drop(render_tx); // render_rx auto shuts down 
+    render_handle.join().unwrap();
+
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    
+    Ok(())
+}
